45ece9cc40fed99088b4ecbe3cf21a5dd7bc2ca1
hansolkim9 <masicer2@gmail.com>
2024-04-18 14:42:02 +0900

    # protected 학습

---
+++ src/day08/protec/pac2/D.java
@@ -0,0 +1,23 @@
+package day08.protec.pac2;
+
+import day08.protec.pac1.A;
+
+public class D extends A {
+
+    D() {
+        super(100);
+
+        f1 = 100;
+//        f2 = 200;
+
+        m1();
+//        m2();
+
+    }
+
+    void test() {
+        A a = new A();
+//        f1 = 10; f2 = 30;
+//        m1(); m2();
+    }
+}
