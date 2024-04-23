666d2893c9b4299d0b7d4ab736c18a1f7a7b2a47
hansolkim9 <masicer2@gmail.com>
2024-04-23 11:04:13 +0900

    # 자바표준 API - lang, time 패키지

---
+++ src/day11/api/lang/Wrapper.java
@@ -0,0 +1,26 @@
+package day11.api.lang;
+
+public class Wrapper {
+
+	private int number1;
+	private Integer number2;
+
+	Wrapper () {
+		this.number2 = 0;
+	}
+
+	public static void main(String[] args) {
+
+		// byte, short, int, long, boolean, float, double, char
+		// primitive type (기본 타입, 기초 타입, 원시 타입)
+
+		// Wrapper type (기본 타입을 객체로 포장함)
+		// Byte, Short, Integer, Long, Boolean, Float, Double, Character
+//		int a = 10;
+		Integer a = 10;
+//		double b = 3.3;
+		Double b = 3.3;
+
+
+	}
+}
